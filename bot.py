81b770460f2f83b6de1e5dbfb2661bb8b55b57fd
egor5q <31898567+egor5q@users.noreply.github.com>
2019-01-03 20:31:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,7 +67,7 @@ def menu1(id,calldata=None,callid=None):
         z=1
         text+=str(i)+' линия:| '
         while z<=x['glenght']:
-            text+=planttoemoji(x['garden-plants'][str(i)+'line'][str(z)+'pos'])+' '
+            text+=planttoemoji(x['garden-plants'][str(i)+'line'][str(z)+'pos'])+'   '
             z+=1
         text+='\n'
         i+=1
@@ -173,7 +173,7 @@ def planttoname(x):
     
 def planttoemoji(x):
     if x==None:
-        return '_'
+        return '__'
     else:
         return em_plants[x]
  
